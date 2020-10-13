cda5b59928e553eef9a5573d0fbb595b2076535e
Karacky <mrnk3078@gmail.com>
2020-10-14 01:14:12 +0900

    トークンにomitemptyを設定

--- pkg/domain/model/user/model.go
+++ pkg/domain/model/user/model.go
@@ -3,6 +3,6 @@ package user
 // User Userを表すドメインモデル
 type User struct {
 	ID        string `json:"id"`
-	AuthToken string `json:"auth_token"`
+	AuthToken string `json:"auth_token,omitempty"`
 	Name      string `json:"name"`
 }
