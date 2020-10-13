64d83b569a0ae4ddc403fd04822befad04fd6eea
Karacky <mrnk3078@gmail.com>
2020-10-14 01:14:37 +0900

    トークンからユーザーを取得するrepositoryを追加

--- pkg/domain/repository/db/user/repository.go
+++ pkg/domain/repository/db/user/repository.go
@@ -1,6 +1,11 @@
 package user
 
+import (
+	"layered-arch-sample/pkg/domain/model/user"
+)
+
 // Repository データ永続化のために抽象化したUserデータ更新周りの処理
 type Repository interface {
 	Create(ID, authToken, name string) error
+	SelectByAuthToken(authToken string) (*user.User, error)
 }
